ea354d91cda7a99cfad520de2e69b4c30284acb2
mark <damark215@gmail.com>
2024-07-05 14:30:56 +0100

    User lists

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,15 +1,29 @@
-import { router } from 'expo-router';
+import { Link, Stack, router } from 'expo-router';
 import {  ChannelList } from 'stream-chat-expo';
 import { useAuth } from '../../../providers/AuthProvider';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
 export default function MainTabScreen() {
     const { user } = useAuth()
 return (
-   
+   <>
+   <Stack.Screen options={{headerRight: () => (
+    
+    <Link href={'/(home)/users'} asChild>
+    <FontAwesome 
+    name="users" 
+    size={22} 
+    color="gray" 
+    style={{ marginHorizontal: 15}} 
+    />
+    </Link>
+   )
+   }}/>
 <ChannelList 
 onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
 filters={{members: { $in: [user.id] } }}
     />
+    </>
 );
 }
